84bbad0518569b3bdaf3e3c768c2260c1c442109
fof-dev <fofanabilali2014@gmail.com>
2021-01-22 09:14:09 +0100

    ok

--- resources/views/pages_backend/commande/facturation.blade.php
+++ resources/views/pages_backend/commande/facturation.blade.php
@@ -109,11 +109,6 @@
 								</tr>
 
                                 <tr scope="col" colspan="5" rowspan="1" class="text-center">
-									<th colspan="3"  style="font-size:17px;">TAXE</th>
-									<th colspan="2"  style="font-size:17px;"> 0 %</th>
-								</tr>
-
-                                <tr scope="col" colspan="5" rowspan="1" class="text-center">
 									<th colspan="3"  style="font-size:17px;">FRAIS DE LIVRAISON</th>
 									<th colspan="2"  style="font-size:17px;"> 0 F CFA</th>
 								</tr>
--- resources/views/pages_frontend/details-historique-achats.blade.php
+++ resources/views/pages_frontend/details-historique-achats.blade.php
@@ -120,10 +120,7 @@ if (Cookie::get('id_user')== null)
 									<th colspan="3"  style="font-size:17px;"> SOUS TOTAL</th>
 									<th colspan="2"  style="font-size:17px;"> <?php echo $prix_total?> F CFA</th>
 								</tr>
-                                <tr scope="col" colspan="5" rowspan="1" class="text-left">
-									<th colspan="3"  style="font-size:17px;">TAXE</th>
-									<th colspan="2"  style="font-size:17px;"> 0 %</th>
-								</tr>
+                                
                                 <tr scope="col" colspan="5" rowspan="1" class="text-left">
 									<th colspan="3"  style="font-size:17px;">FRAIS DE LIVRAISON</th>
 									<th colspan="2"  style="font-size:17px;"> 0 F CFA</th>
